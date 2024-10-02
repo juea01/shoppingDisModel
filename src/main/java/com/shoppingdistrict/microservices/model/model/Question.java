e2b83de7a731d996bf7f305e2f3e63d4622c97a3
juea01 <akashwarkalay@gmail.com>
2024-10-02 14:14:17 +1300

    question type and timer fields added in question model class

--- src/main/java/com/shoppingdistrict/microservices/model/model/Question.java
+++ src/main/java/com/shoppingdistrict/microservices/model/model/Question.java
@@ -48,6 +48,13 @@ public class Question {
 	// here
 	@JsonIgnore
 	private List<CompletedQuestion> completedQuestions;
+	
+	@Column(name = "question_type")
+	@Size(min = 2, max = 30, message = "Question type should have at least 2 characters and no more than 30 characters")
+	private String questionType;
+	
+	@Column(name = "allocated_time")
+	private int allocatedTime;
 
 	public Question() {
 
@@ -55,7 +62,7 @@ public class Question {
 
 	public Question(int id,
 			@Size(min = 3, max = 250, message = "Content should have at least 3 characters and no more than 250 characters") String content,
-			List<QuestionOption> options, Articles article, Subject subject, List<CompletedQuestion> completedQuestions) {
+			List<QuestionOption> options, Articles article, Subject subject, List<CompletedQuestion> completedQuestions, String questionType, int allocatedTime) {
 		super();
 		this.id = id;
 		this.content = content;
@@ -63,6 +70,8 @@ public class Question {
 		this.article = article;
 		this.subject = subject;
 		this.completedQuestions = completedQuestions;
+		this.questionType = questionType;
+		this.allocatedTime = allocatedTime;
 	}
 
 	public int getId() {
@@ -112,7 +121,22 @@ public class Question {
 	public void setCompletedQuestions(List<CompletedQuestion> completedQuestions) {
 		this.completedQuestions = completedQuestions;
 	}
-	
+
+	public String getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(String questionType) {
+		this.questionType = questionType;
+	}
+
+	public int getAllocatedTime() {
+		return allocatedTime;
+	}
+
+	public void setAllocatedTime(int allocatedTime) {
+		this.allocatedTime = allocatedTime;
+	}
 	
 
 }
